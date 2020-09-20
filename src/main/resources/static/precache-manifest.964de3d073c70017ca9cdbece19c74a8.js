a5bd4aff8659d33ec472131e7015c9706bc3137d
Heidi Sheng <heidisheng@MacBook-Air.local>
2020-09-20 18:16:59 -0400

    with react

---
+++ src/main/resources/static/precache-manifest.964de3d073c70017ca9cdbece19c74a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "428ee45cd47f2dce677e61cd15dad7e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8052d50efff24bf56dda",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a7bed5664a6f62d3a7bb",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "8052d50efff24bf56dda",
+    "url": "/static/js/2.ae13dadd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ae13dadd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7bed5664a6f62d3a7bb",
+    "url": "/static/js/main.21de410f.chunk.js"
+  },
+  {
+    "revision": "f0395242fcfe40790f57",
+    "url": "/static/js/runtime-main.85b40700.js"
+  }
+]);
